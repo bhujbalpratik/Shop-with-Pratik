0e714a4a36610bfe22d31d92a825dfa7a534dc29
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-04-22 20:02:18 +0530

    Navigation component updated

--- client/src/customer/components/navigation/Navigation.jsx
+++ client/src/customer/components/navigation/Navigation.jsx
@@ -7,136 +7,8 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline"
 import { navigation } from "./navigationData"
-
-// const navigation = {
-//   categories: [
-//     {
-//       id: "women",
-//       name: "Women",
-//       featured: [
-//         {
-//           name: "New Arrivals",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
-//           imageAlt:
-//             "Models sitting back to back, wearing Basic Tee in black and bone.",
-//         },
-//         {
-//           name: "Basic Tees",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
-//           imageAlt:
-//             "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
-//         },
-//       ],
-//       sections: [
-//         {
-//           id: "clothing",
-//           name: "Clothing",
-//           items: [
-//             { name: "Tops", href: "#" },
-//             { name: "Dresses", href: "#" },
-//             { name: "Pants", href: "#" },
-//             { name: "Denim", href: "#" },
-//             { name: "Sweaters", href: "#" },
-//             { name: "T-Shirts", href: "#" },
-//             { name: "Jackets", href: "#" },
-//             { name: "Activewear", href: "#" },
-//             { name: "Browse All", href: "#" },
-//           ],
-//         },
-//         {
-//           id: "accessories",
-//           name: "Accessories",
-//           items: [
-//             { name: "Watches", href: "#" },
-//             { name: "Wallets", href: "#" },
-//             { name: "Bags", href: "#" },
-//             { name: "Sunglasses", href: "#" },
-//             { name: "Hats", href: "#" },
-//             { name: "Belts", href: "#" },
-//           ],
-//         },
-//         {
-//           id: "brands",
-//           name: "Brands",
-//           items: [
-//             { name: "Full Nelson", href: "#" },
-//             { name: "My Way", href: "#" },
-//             { name: "Re-Arranged", href: "#" },
-//             { name: "Counterfeit", href: "#" },
-//             { name: "Significant Other", href: "#" },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       id: "men",
-//       name: "Men",
-//       featured: [
-//         {
-//           name: "New Arrivals",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
-//           imageAlt:
-//             "Drawstring top with elastic loop closure and textured interior padding.",
-//         },
-//         {
-//           name: "Artwork Tees",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg",
-//           imageAlt:
-//             "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
-//         },
-//       ],
-//       sections: [
-//         {
-//           id: "clothing",
-//           name: "Clothing",
-//           items: [
-//             { name: "Tops", href: "#" },
-//             { name: "Pants", href: "#" },
-//             { name: "Sweaters", href: "#" },
-//             { name: "T-Shirts", href: "#" },
-//             { name: "Jackets", href: "#" },
-//             { name: "Activewear", href: "#" },
-//             { name: "Browse All", href: "#" },
-//           ],
-//         },
-//         {
-//           id: "accessories",
-//           name: "Accessories",
-//           items: [
-//             { name: "Watches", href: "#" },
-//             { name: "Wallets", href: "#" },
-//             { name: "Bags", href: "#" },
-//             { name: "Sunglasses", href: "#" },
-//             { name: "Hats", href: "#" },
-//             { name: "Belts", href: "#" },
-//           ],
-//         },
-//         {
-//           id: "brands",
-//           name: "Brands",
-//           items: [
-//             { name: "Re-Arranged", href: "#" },
-//             { name: "Counterfeit", href: "#" },
-//             { name: "Full Nelson", href: "#" },
-//             { name: "My Way", href: "#" },
-//           ],
-//         },
-//       ],
-//     },
-//   ],
-//   pages: [
-//     { name: "Company", href: "#" },
-//     { name: "Stores", href: "#" },
-//   ],
-// }
+import { Avatar, Menu, MenuItem } from "@mui/material"
+import { deepPurple } from "@mui/material/colors"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
@@ -145,6 +17,11 @@ function classNames(...classes) {
 const Navigation = () => {
   const [open, setOpen] = useState(false)
 
+  const [anchorEl, setAnchorEl] = useState(null)
+  const openUserMenu = Boolean(anchorEl)
+
+  const handleCloseUserMenu = () => {}
+  const handleUserClick = (e) => setAnchorEl(e.currentTarget)
   return (
     <div className="bg-white">
       {/* Mobile menu */}
@@ -325,7 +202,7 @@ const Navigation = () => {
 
       <header className="relative bg-white">
         <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-          Get free delivery on orders over $100
+          Get free delivery on orders over &#8377;500
         </p>
 
         <nav
@@ -349,8 +226,8 @@ const Navigation = () => {
                 <a href="#">
                   <span className="sr-only">Your Company</span>
                   <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    className="h-10 w-auto"
+                    src="LogoE-commerce.png"
                     alt=""
                   />
                 </a>
@@ -482,34 +359,45 @@ const Navigation = () => {
 
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-gray-700 hover:text-gray-800"
-                  >
-                    Sign in
-                  </a>
-                  <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-gray-700 hover:text-gray-800"
-                  >
-                    Create account
-                  </a>
-                </div>
+                  {true ? (
+                    <div>
+                      <Avatar
+                        className="text-white"
+                        onClick={handleUserClick}
+                        aria-controls={open ? "basic-menu" : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? "true" : undefined}
+                        sx={{
+                          bgcolor: deepPurple[500],
+                          color: "white",
+                          cursor: "pointer",
+                        }}
+                      >
+                        P
+                      </Avatar>
 
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a
-                    href="#"
-                    className="flex items-center text-gray-700 hover:text-gray-800"
-                  >
-                    <img
-                      src="https://tailwindui.com/img/flags/flag-canada.svg"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-sm font-medium">CAD</span>
-                    <span className="sr-only">, change currency</span>
-                  </a>
+                      <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={openUserMenu}
+                        onClose={handleCloseUserMenu}
+                        MenuListProps={{ "aria-labelledby": "basic-button" }}
+                      >
+                        <MenuItem onClick={handleCloseUserMenu}>
+                          Profile
+                        </MenuItem>
+                        <MenuItem>My Orders</MenuItem>
+                        <MenuItem>Logout</MenuItem>
+                      </Menu>
+                    </div>
+                  ) : (
+                    <Button
+                      onClick={handleOpen}
+                      className="text-sm font-medium text-gray-700 hover:text-gray-900"
+                    >
+                      Signin
+                    </Button>
+                  )}
                 </div>
 
                 {/* Search */}
