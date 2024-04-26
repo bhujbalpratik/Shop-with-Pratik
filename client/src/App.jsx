073f409662cf23b7799daf2c2f3dc3ce76fd3dd2
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-04-26 22:11:03 +0530

    Product detail page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./customer/components/Footer/Footer"
 import Navigation from "./customer/components/navigation/Navigation"
 import Product from "./customer/components/Product/Product"
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails"
 import Home from "./customer/pages/Home/Home"
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
       <Navigation />
       <div className="">
         {/* <Home /> */}
-        <Product />
+        {/* <Product /> */}
+        <ProductDetails />
       </div>
       <Footer />
     </div>
