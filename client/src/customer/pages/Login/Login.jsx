0bcb6b8610e0eb525e214f5cd75a4a6d8b28f088
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-05-07 16:47:32 +0530

    Login page ui created

--- client/src/customer/pages/Login/Login.jsx
+++ client/src/customer/pages/Login/Login.jsx
@@ -1,4 +1,70 @@
-const Login = () => {
-  return <div>Login</div>
+import { Link } from "react-router-dom"
+
+export default function Login() {
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-3 py-8 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto lg:h-20"
+            src="LogoE-commerce.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            LOGIN
+          </h2>
+        </div>
+
+        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-5">
+            <div>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  placeholder="Email address"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset "
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-3 text-md font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 uppercase"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Don't have an account?{" "}
+            <Link
+              to={"/signup"}
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
-export default Login
